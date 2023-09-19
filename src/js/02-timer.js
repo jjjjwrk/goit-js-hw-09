38812b19410df5720dd782b216db780c4414e049
jjjjwrk <130352649+jjjjwrk@users.noreply.github.com>
2023-09-18 18:13:28 -0700

    done

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,77 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 
-const input = document.querySelector('#datetime-picker');
+const userInput = document.querySelector('#datetime-picker');
 const startBtn = document.querySelector('[data-start]');
-const days = document.querySelector('[data-days]');
-const hours = document.querySelector('[data-hours]');
-const minutes = document.querySelector('[data-minutes]');
-const seconds = document.querySelector('[data-seconds]');
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-      onClose(selectedDates) {
-          checkDate(selectedDates);
-      },
-  };
+const inputDays = document.querySelector('[data-days]');
+const inputHours = document.querySelector('[data-hours]');
+const inputMinutes = document.querySelector('[data-minutes]');
+const inputSeconds = document.querySelector('[data-seconds]');
 
 startBtn.disabled = true;
 
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        console.log(selectedDates[0]);
+        checkDate(selectedDates);
+    },
+};
+
+function checkDate(selectedDates) {
+    const selectDate = selectedDates[0];
+    const currentDate = options.defaultDate;
+    if (selectDate <= currentDate) {
+            alert('Please choose a date in the future');
+            return;
+    };
+    startBtn.disabled = false;
+    
+    startBtn.addEventListener('click', onClickStart);
+    function onClickStart() {
+        startBtn.disabled = true;
+        userInput.disabled = true;
+        timerOn(selectDate);
+    };
+};
+
+flatpickr(userInput, options);
+
+function timerOn(selectDate) {
+    const timerID = setInterval(() => {
+        let currentDate = new Date();
+        let resData = selectDate - currentDate;
+        if (resData <= 0) {
+            clearInterval(timerID);
+            userInput.disabled = false;
+            alert('Time is over');
+            return;
+        };
+        updateTimer(convertMs(resData));
+    }, 1000);
+};
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function updateTimer(convertData) {
+    const { days, hours, minutes, seconds } = convertData;
+    inputDays.textContent = days;
+    inputHours.textContent = hours;
+    inputMinutes.textContent = minutes;
+    inputSeconds.textContent = seconds;
+};
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay })
+      } else {
+        // Reject
+        reject ({ position, delay });
+      }
+    }, delay);
+  })
 }
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event){
+  event.preventDefault();
+  let userDelay = Number(event.currentTarget.delay.value);
+  let step = Number(event.currentTarget.step.value);
+  let amount = Number(event.currentTarget.amount.value);
+
+  if(userDelay < 0 || step < 0 || amount <= 0){
+    alert('error');
+    event.currentTarget.reset();
+    return;
+  }
+
+  for(let i = 1; i <= amount; i += 1){
+    createPromise(i, userDelay).then(({ position, delay }) => {
+      console.log('✅ Fulfilled promise ${position} in ${delay}ms')
+    }).catch(({ position, delay }) => {
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+    userDelay += step;
+  };
+  event.currentTarget.reset();
+};
+
